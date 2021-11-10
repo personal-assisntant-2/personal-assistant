8bf7ff3799cecce15c0074a90f326d8085b95b27
Yana Anisimova <begemotvot2@gmail.com>
2021-11-10 14:14:35 +0200

    news with comments + wrappered coroutine in functools.partial()

--- assistant/news/settings.py
+++ assistant/news/settings.py
@@ -1,5 +1,6 @@
-from .handlers import client_bitinfo, client_bitstat, client_myfin
+import functools
 
+from .handlers import client_bitinfo, client_bitstat, client_myfin
 
 
 # Resources
@@ -15,9 +16,30 @@ URL_BITCOINCASH_BITSTAT = 'https://bitstat.top/coin.php?id_coin=11'
 
 
 # LIST URL in this order: ['bitcoin', 'ethereum', 'bitcoin_cash']!!!
+# functools.partial(func, *args, **keywords) - returns a partial-object (in fact, a function), which when called is
+# called as a 'func', but the arguments are passed there.
+# 'func' - coroutine.
 handlers = {
-    'rates_bitstat': client_bitstat([URL_BITCOIN_BITSTAT, URL_ETHEREUM_BITSTAT, URL_BITCOINCASH_BITSTAT]),
-    'rates_myfin': client_myfin([URL_BITCOIN_MYFIN, URL_ETHEREUM_MYFIN, URL_BITCOINCASH_MYFIN]),
-    'rates_bitinfo': client_bitinfo([URL_BITCOIN_BITINFOCHARTS, URL_ETHEREUM_BITINFOCHARTS, URL_BITCOINCASH_BITINFOCHARTS]),
-     }
+    'rates_bitstat': functools.partial(
+        client_bitstat, [
+            URL_BITCOIN_BITSTAT,
+            URL_ETHEREUM_BITSTAT,
+            URL_BITCOINCASH_BITSTAT
+        ]
+    ),
+    'rates_myfin': functools.partial(
+        client_myfin, [
+            URL_BITCOIN_MYFIN,
+            URL_ETHEREUM_MYFIN,
+            URL_BITCOINCASH_MYFIN
+        ]
+    ),
+    'rates_bitinfo': functools.partial(
+        client_bitinfo, [
+            URL_BITCOIN_BITINFOCHARTS,
+            URL_ETHEREUM_BITINFOCHARTS,
+            URL_BITCOINCASH_BITINFOCHARTS
+        ]
+    ),
+}
 
