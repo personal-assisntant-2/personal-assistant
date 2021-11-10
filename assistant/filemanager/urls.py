02a8e943cf4a245f2a73e6ddb31f0da0369bcf8c
Yana Anisimova <begemotvot2@gmail.com>
2021-11-10 21:24:16 +0200

    add delete file

--- assistant/filemanager/urls.py
+++ assistant/filemanager/urls.py
@@ -8,4 +8,5 @@ app_name = 'file_manager'
 urlpatterns = [
     path('', views.file_manager_view, name='file'),
     path('download/<int:file_id>/', views.file_download_view, name='download'),
+    path('delete/<int:file_id>/', views.file_delete_view, name='delete'),
     ]
